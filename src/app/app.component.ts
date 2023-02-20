f1e5786c905bccf9df37254cb0b5dfafd6d46a81
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-19 19:26:00 -0600

    Update the decorator's elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 
-@Component({
+/*@Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Hello world</h1><h3>Using my own inline template</h3>`,
   styleUrls: ['./app.component.scss']
+})*/
+@Component({
+  selector: 'jahm-root', // Selector name comes from angular.json
+  template: `<h1>Hello world</h1><h3>Using my own inline template</h3>`, // Defining an inline template
+  styles: [`h1{color: darkred;} h3{color: orangered}`] // Defining an inline style
 })
 export class AppComponent {
   title = 'HotelInventory';
