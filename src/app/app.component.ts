a5c37f6adc883686335cda3423a7edee4098ac2e
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-19 21:08:30 -0600

    Update the Rooms component template URL and selector

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component } from '@angular/core';
 
-/*@Component({
-  selector: 'app-root',
-  template: `<h1>Hello world</h1><h3>Using my own inline template</h3>`,
-  styleUrls: ['./app.component.scss']
-})*/
 @Component({
+  selector: 'jahm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+/*@Component({
   selector: 'jahm-root', // Selector name comes from angular.json
   template: `<h1>Hello world</h1><h3>Using my own inline template</h3>`, // Defining an inline template
   styles: [`h1{color: darkred;} h3{color: orangered}`] // Defining an inline style
-})
+})*/
 export class AppComponent {
   title = 'HotelInventory';
 }
