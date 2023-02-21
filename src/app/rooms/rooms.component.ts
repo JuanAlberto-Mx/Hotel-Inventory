493f1a7b9d06765e32d7a2985ad912e8da85c053
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-21 09:51:32 -0600

    Update RoomsComponent class business logic to use the Room and RoomList interfaces

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Room, RoomList} from "./rooms";
 
 @Component({
   selector: 'jahm-rooms',
@@ -14,6 +15,47 @@ export class RoomsComponent implements OnInit{
   // Properties to manage front-end elements
   hideRooms = false;
   toggleText = "Hide";
+
+  // Implementation of a room based on the Room interface
+  rooms: Room = {
+    totalRooms: 103,
+    availableRooms: 47,
+    bookedRooms: 56
+  };
+
+  // Implementation of a room list based on the RoomList interface
+  roomList: RoomList[] = [
+    {
+      roomNumber: 102,
+      roomType:"King-Double",
+      amenities:"Air conditioner, Coffee, Free Wi-Fi, TV, Bathroom",
+      price: 1500,
+      photos: "king-double.jpg",
+      checkinTime: new Date('01-Feb-2023'),
+      checkoutTime: new Date('20-Feb-2023'),
+      rating: 4.5
+    },
+    {
+      roomNumber: 232,
+      roomType:"Junior Suite",
+      amenities:"Air conditioner, Coffee, Free Wi-Fi, TV, Bathroom, Kitchen",
+      price: 2000,
+      photos: "JuniorSuite.jpg",
+      checkinTime: new Date('20-Feb-2023'),
+      checkoutTime: new Date('10-Mar-2023'),
+      rating: 5.0
+    },
+    {
+      roomNumber: 301,
+      roomType:"Single",
+      amenities:"Air conditioner, Free Wi-Fi, TV, Bathroom",
+      price: 2000,
+      photos: "JuniorSuite.jpg",
+      checkinTime: new Date('20-Feb-2023'),
+      checkoutTime: new Date('28-Feb-2023'),
+      rating: 2.6
+    }
+  ]
 
   constructor() {
   }
