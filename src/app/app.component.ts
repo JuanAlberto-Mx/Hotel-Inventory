0e87a5c2a1df2ee97e03581090d1d1e546fa5265
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-22 12:58:34 -0600

    Add ViewChild decorators to reference the created templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {RoomsComponent} from "./rooms/rooms.component";
 
 @Component({
   selector: 'jahm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-/*@Component({
-  selector: 'jahm-root', // Selector name comes from angular.json
-  template: `<h1>Hello world</h1><h3>Using my own inline template</h3>`, // Defining an inline template
-  styles: [`h1{color: darkred;} h3{color: orangered}`] // Defining an inline style
-})*/
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'HotelInventory';
   role = 'Admin';
+
+  // user variable is defined in ng-template
+  @ViewChild('user', {read: ViewContainerRef}) userRef!: ViewContainerRef;
+
+  // name variable to reference the template
+  @ViewChild('name', {static: true}) nameRef!: ElementRef;
+
+  ngOnInit(): void {
+    // Access to innerText property by using nativeElement
+    this.nameRef.nativeElement.innerText = "Holiday Inn Orizaba";
+  }
+
+  /*
+  ngAfterViewInit(): void {
+    // Create the reference to the RoomsComponent
+    const componentRef = this.userRef.createComponent(RoomsComponent);
+
+    // Modify the numberOfRooms property
+    componentRef.instance.numberOfRooms = 103;
+  }*/
 }
