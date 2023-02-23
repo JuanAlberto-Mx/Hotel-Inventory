2b619caa0ce122ed358b3a365e2e58d91fe5d47d
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-22 21:00:01 -0600

    Add Container component business logic.

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,23 @@
+import {AfterContentInit, Component, ContentChild, OnInit} from '@angular/core';
+import {EmployeeComponent} from "../employee/employee.component";
+
+@Component({
+  selector: 'jahm-container',
+  templateUrl: './container.component.html',
+  styleUrls: ['./container.component.scss']
+})
+export class ContainerComponent implements OnInit, AfterContentInit {
+
+  @ContentChild(EmployeeComponent) employeeContentChild!: EmployeeComponent;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterContentInit(): void {
+    this.employeeContentChild.employeeName = "John Albert";
+  }
+
+}
