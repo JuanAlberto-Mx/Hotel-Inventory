405a695fcd280cf700ab45939d251aa30d778360
Juan-Alberto Hernández-Martínez <jahm.developer@gmail.com>
2023-02-23 11:28:22 -0600

    Add dependency injection and providers to use the RoomsService service.

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,15 +1,17 @@
 import {Component, OnInit} from '@angular/core';
+import {RoomsService} from "../rooms/services/rooms.service";
 
 @Component({
   selector: 'jahm-employee',
   templateUrl: './employee.component.html',
-  styleUrls: ['./employee.component.scss']
+  styleUrls: ['./employee.component.scss'],
+  providers: [RoomsService] // RoomsService local instance used
 })
 export class EmployeeComponent implements OnInit {
 
   employeeName: string = "Albert";
 
-  constructor() {
+  constructor(private roomsService: RoomsService) {
   }
 
   ngOnInit(): void {
